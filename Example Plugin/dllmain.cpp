bb62255591a2d0b730afccf6b63ae2d7390d6c50
ElusiveFluffy <Freya8822@hotmail.com>
2024-09-06 14:28:30 +0930

    Add Imgui and WndProc API, and add a cleaner way to run most functions

--- Example Plugin/dllmain.cpp
+++ Example Plugin/dllmain.cpp
@@ -2,8 +2,10 @@
 #include "pch.h"
 #include "API.hpp"
 #include <fstream>
+#include "imgui.h"
 
 using namespace TygerFrameworkAPI; // For API class
+static bool init;
 
 BOOL APIENTRY DllMain( HMODULE hModule,
                        DWORD  ul_reason_for_call,
@@ -17,15 +19,39 @@ BOOL APIENTRY DllMain( HMODULE hModule,
     return TRUE;
 }
 
+void Initialize() {
+    ImGui::SetCurrentContext((ImGuiContext*)API::GetImGuiContext());
+    init = true;
+}
+
+void DrawUI() {
+    if (!init)
+        Initialize();
+
+    if (ImGui::Begin("Example Plugin"));
+        ImGui::Text("Hello World");
+
+    ImGui::End();
+}
+
+bool WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam) {
+
+    return false;
+}
+
+
 
 EXTERN_C bool TygerFrameworkPluginInitialize(TygerFrameworkPluginInitializeParam* param) {
     //Make sure to call this first before any API Functions
     API::Initialize(param);
 
-    API::Get()->LogPluginMessage("Hello World From Example Plugin!");
-    API::Get()->LogPluginMessage("Plugin Received Ty " + std::to_string(param->functions->WhichTyGame()) + " as the Current Game");
-    API::Get()->LogPluginMessage("Warning Test", Warning);
-    API::Get()->LogPluginMessage("Error Test", Error);
+    param->functions->DrawPluginUI(DrawUI);
+    param->functions->PluginWndProc((WndProcFunc)WndProc);
+
+    API::LogPluginMessage("Hello World From Example Plugin!");
+    API::LogPluginMessage("Plugin Received Ty " + std::to_string(API::WhichTyGame()) + " as the Current Game");
+    API::LogPluginMessage("Warning Test", Warning);
+    API::LogPluginMessage("Error Test", Error);
     return true;
 }
 
