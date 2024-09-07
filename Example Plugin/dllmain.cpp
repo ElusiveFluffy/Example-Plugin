a09c92a9c1cd29d142b8242e747ced37e33c33c8
ElusiveFluffy <Freya8822@hotmail.com>
2024-09-07 12:21:32 +0930

    Add API function to set the plugin imgui font to match the loader

--- Example Plugin/dllmain.cpp
+++ Example Plugin/dllmain.cpp
@@ -26,12 +26,16 @@ void Initialize() {
     //Setup ImGui Context
     ImGui::CreateContext();
 
+    //Set the font to be the same as TygerFramework
+    API::SetImGuiFont(ImGui::GetIO().Fonts);
+
     ImGuiIO& io = ImGui::GetIO();
     (void)io;
     if (API::GetTyWindowHandle() == 0) {
         API::LogPluginMessage("Error");
         return;
     }
+    //Setup backend
     ImGui_ImplWin32_InitForOpenGL(API::GetTyWindowHandle());
     ImGui_ImplOpenGL3_Init();
 
@@ -39,6 +43,7 @@ void Initialize() {
     init = true;
 }
 
+//ImGui Drawing code
 void DrawUI() {
     if (!init)
         Initialize();
@@ -55,6 +60,7 @@ void DrawUI() {
     ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
 }
 
+//WndProc to be able to interact with imgui or block any WndProc from interacting with the Ty window
 extern LRESULT ImGui_ImplWin32_WndProcHandler(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam);
 bool WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam) {
     if (ImGui_ImplWin32_WndProcHandler(hWnd, msg, wParam, lParam))
@@ -62,6 +68,7 @@ bool WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam) {
     return false;
 }
 
+//To block clicks from the game when the window is focused
 bool ImGuiHasFocus() {
     return ImGui::IsWindowFocused(ImGuiFocusedFlags_AnyWindow);
 }
@@ -70,10 +77,13 @@ EXTERN_C bool TygerFrameworkPluginInitialize(TygerFrameworkPluginInitializeParam
     //Make sure to call this first before any API Functions
     API::Initialize(param);
 
+    //Subcribe functions to TygerFramework events
     param->functions->DrawPluginUI(DrawUI);
+    //Make sure to cast these, otherwise TygerFramework won't get the return value
     param->functions->PluginImGuiHasFocus((ImGuiHasFocusFunc)ImGuiHasFocus);
     param->functions->PluginWndProc((WndProcFunc)WndProc);
 
+    //Example log messages
     API::LogPluginMessage("Hello World From Example Plugin!");
     API::LogPluginMessage("Plugin Received Ty " + std::to_string(API::WhichTyGame()) + " as the Current Game");
     API::LogPluginMessage("Warning Test", Warning);
