bd09a570965e0c9a8afaee17d53c8815b0edd610
ElusiveFluffy <Freya8822@hotmail.com>
2024-09-07 11:50:12 +0930

    Change each plugin to have their own imgui context (so when the imgui versions don't match it doesn't crash)

--- Example Plugin/dllmain.cpp
+++ Example Plugin/dllmain.cpp
@@ -3,6 +3,8 @@
 #include "API.hpp"
 #include <fstream>
 #include "imgui.h"
+#include "imgui_impl_opengl3.h"
+#include "imgui_impl_win32.h"
 
 using namespace TygerFrameworkAPI; // For API class
 static bool init;
@@ -20,7 +22,20 @@ BOOL APIENTRY DllMain( HMODULE hModule,
 }
 
 void Initialize() {
-    ImGui::SetCurrentContext((ImGuiContext*)API::GetImGuiContext());
+
+    //Setup ImGui Context
+    ImGui::CreateContext();
+
+    ImGuiIO& io = ImGui::GetIO();
+    (void)io;
+    if (API::GetTyWindowHandle() == 0) {
+        API::LogPluginMessage("Error");
+        return;
+    }
+    ImGui_ImplWin32_InitForOpenGL(API::GetTyWindowHandle());
+    ImGui_ImplOpenGL3_Init();
+
+    API::LogPluginMessage("Initialized ImGui");
     init = true;
 }
 
@@ -28,24 +43,35 @@ void DrawUI() {
     if (!init)
         Initialize();
 
-    if (ImGui::Begin("Example Plugin"));
-        ImGui::Text("Hello World");
+    ImGui_ImplOpenGL3_NewFrame();
+    ImGui_ImplWin32_NewFrame();
+    ImGui::NewFrame();
+    ImGui::Begin("Example Plugin");
+    ImGui::Text("Hello World");
 
     ImGui::End();
+
+    ImGui::Render();
+    ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
 }
 
+extern LRESULT ImGui_ImplWin32_WndProcHandler(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam);
 bool WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam) {
-
+    if (ImGui_ImplWin32_WndProcHandler(hWnd, msg, wParam, lParam))
+        return true;
     return false;
 }
 
-
+bool ImGuiHasFocus() {
+    return ImGui::IsWindowFocused(ImGuiFocusedFlags_AnyWindow);
+}
 
 EXTERN_C bool TygerFrameworkPluginInitialize(TygerFrameworkPluginInitializeParam* param) {
     //Make sure to call this first before any API Functions
     API::Initialize(param);
 
     param->functions->DrawPluginUI(DrawUI);
+    param->functions->PluginImGuiHasFocus((ImGuiHasFocusFunc)ImGuiHasFocus);
     param->functions->PluginWndProc((WndProcFunc)WndProc);
 
     API::LogPluginMessage("Hello World From Example Plugin!");
